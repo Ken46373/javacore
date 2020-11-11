e6d502c7909403987ea30e110703d7c989d3d544
Ken Minami <kenminami.dev@gmail.com>
2020-11-11 21:19:41 +0900

    hello world.

--- src/javacore/presentations/DispatcherServlet.java
+++ src/javacore/presentations/DispatcherServlet.java
@@ -1,10 +1,25 @@
 package javacore.presentations;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 /**
  * Servletクラス
  *
  * @author kenminami
  */
-public class DispatcherServlet {
+@WebServlet("/helloworld")
+public class DispatcherServlet extends HttpServlet {
 
+	private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String param = request.getParameter("param");
+        response.getWriter().write("Hello "+ param);
+    }
 }
