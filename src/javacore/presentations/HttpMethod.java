7448b96fcaaf05b6b765641b52fb7b4f190eb36c
Ken Minami <kenminami.dev@gmail.com>
2020-11-18 20:59:09 +0900

    HttpMethod実装終了

--- src/javacore/presentations/HttpMethod.java
+++ src/javacore/presentations/HttpMethod.java
@@ -10,13 +10,36 @@ import java.util.Map;
  */
 public enum HttpMethod {
 
-  GET, POST, PUT, DELETE, PATCH, ANY;
+  GET, POST, PUT, DELETE, PATCH, HEAD, OPTIONS,TRACE, ANY;
 
+  /** HTTPメソッドマップ */
   private static final Map<String, HttpMethod> HTTP_MAP = new HashMap<String, HttpMethod>();
 
   static {
     for (HttpMethod method : values()) {
       HTTP_MAP.put(method.name(), method);
     }
+  }
+
+  /**
+   * HTTPメソッドの名前を取得する。
+   *
+   * @param method HTTPメソッド文字列
+   * @return HTTPメソッド名
+   */
+  public static HttpMethod resolve(String method) {
+
+    return (method != null ? HTTP_MAP.get(method) : null);
+  }
+
+  /**
+   * HTTPメソッドに該当するかどうかを判定する。
+   *
+   * @param method HTTPメソッド文字列
+   * @return 該当するかどうか
+   */
+  public boolean matches(String method) {
+
+    return (this == resolve(method));
   }
 }
